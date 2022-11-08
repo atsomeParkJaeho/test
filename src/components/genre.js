f8bfb8ae14781a601dbffc6a5c114b70420b66a5
DESKTOP-PQP3R44\chosun <woghsla85@naver.com>
2022-11-08 20:36:04 +0900

    장르별 검색

--- src/components/genre.js
+++ src/components/genre.js
@@ -1,57 +1,66 @@
 import React, {useEffect, useState} from "react";
-import {Link} from "react-router-dom";
 import axios from "axios";
+import {Link, useHistory} from "react-router-dom";
 import {Genre_ids} from "../codeset/genrecode";
-import TodayBoxoffice from "./TodayBoxoffice";
+
 
 function Genre() {
 
-    console.log('장르별 검색 페이지');
-    // 1. 클릭시 장르 아이디 저장
-    const goGenre = Ids => {
-        setgenIds(Ids);
-    };
-    // 2. 장르값 저장
-    const [genIds, setgenIds] = useState([]);
+    // 로컬 스토리지 저장
+    const histtory = useHistory();
+    const inputValue = histtory.location.state;
 
-    console.log('장르값 아이디 / '+ genIds);
-    // 3. 클릭한 장르 아이디로 출력
+    if(inputValue) {
+        localStorage.setItem("Genre",JSON.stringify(inputValue));
+    }
+    // 1. 장르별 데이터 불러오기 설정
+    // 2. 클릭시 해당 장르값 데이터 불러오기 액션 구현
+    // 3. 장르 코드값 너기
+
+    // 클릭시 해당장르 출력 구현
+
+    const goGenre = Ids => {
+        setGenresIds(Ids);
+        alert('추출 완료');
+    }
+    // 클릭한 장르 외부로 추출
+    const [GenresIds, setGenresIds] = useState([]);
+
+    // 장르 불러오기
     const [Genre, setGenre] = useState([]);
-    
-    // 2. 장르 영화 불러오기
     useEffect(()=>{
-        axios.get('/api/UTIL_rating.php',{
+        axios.get('/atnode/api/UTIL_genres.php',{
             params:{
-                act_type:"genre",       // 평점순 출력
-                genre_ids:genIds      // 나라별 출력
+                act_type    :"genres",      // 장르별로 가져오기
+                genres_ids  :"28"           // 해당장르 아이디를 넣는다
             }
         }).then((res)=>{
             if(res) {
-                if(res) {
-                    console.log('데이터 추출 / '+ res);
-                    const {results} = res.data;
-                    setGenre(results);
-                }
+                console.log('데이터 추출 / '+ res);
+                const {results} = res.data;
+                setGenre(results);
             }
         });
     },[]);
 
-    // 데이터 확인
-    console.log(Genre);
+    console.log('데이터 추출 / ' + Genre);
+    console.log('데이터 코드 / ' + GenresIds)
 
     return(
         <>
-            {/*모바일*/}
+            {/*=================모바일=================*/}
             <div className="d-md-none">
                 <div className="overflow-auto">
                     <ul className="d-flex">
                         {Genre_ids.map(cate=>(
-                        <li><Link to="/genre" onClick={()=>goGenre(cate.id)} className="d-block text-dark px-3 py-2">{cate.name}</Link></li>
+                        <li>
+                             <button to="/genre" onClick={()=>goGenre(cate.id)} className="d-block text-dark px-3 py-2">{cate.name}</button>
+                        </li>
                         ))}
                     </ul>
                 </div>
             </div>
-            {/*검색창*/}
+            {/*=================검색창=================*/}
             <div className="input_div bg-primary py-10">
                 <div className="container">
                     <div className="px-3 d-flex justify-content-between align-items-center">
@@ -65,7 +74,7 @@ function Genre() {
                     </div>
                 </div>
             </div>
-            {/*영화 출력 리스트*/}
+            {/*==================영화 출력 리스트============*/}
             <section className="section">
                 <div className="container">
                     <div className="row section-heading justify-content-center text-center  wow fadeInUp">
@@ -78,27 +87,28 @@ function Genre() {
                     <div className="row gy-4">
                         {/*===============반복문 구간===================*/}
                         {Genre.map(items=>(
-                            <div className="col-md-6 col-lg-4 mb-5">
-                                <div className="hover-top card shadow-only-hover">
-                                    <div className="img_box position-relative">
-                                        <img className="rounded" src={`http://image.tmdb.org/t/p/w500/${items.poster_path}`} title="" alt=""/>
-                                    </div>
-                                    <div className="card-body p-3">
-                                        <label className="small mb-2"></label>
-                                        <h5 className="mb-3">
-                                            <Link className="text-dark stretched-link d-block text-truncate" to="#">
-                                                {items.title}
-                                            </Link>
-                                        </h5>
-                                        <p></p>
-                                        <div className="nav small border-top pt-3">
-                                            <Link className="me-2 text-body" to="#"><i className="bi-calendar me-1"></i>개봉일 : {items.release_date}</Link>
-                                            <Link className="text-body fw-600 ms-auto" to="#">더보기<i className="bi-chevron-right"></i></Link>
-                                        </div>
+                        <div className="col-md-6 col-lg-4 mb-5">
+                            <div className="hover-top card shadow-only-hover">
+                                <div className="img_box position-relative">
+                                    <img className="rounded" src={`http://image.tmdb.org/t/p/w500/${items.poster_path}`} title="" alt=""/>
+                                </div>
+                                <div className="card-body p-3">
+                                    <label className="small mb-2">예매순위 {}</label>
+                                    <h5 className="mb-3">
+                                        <Link className="text-dark stretched-link d-block text-truncate" to="#">
+                                            {items.title}
+                                        </Link>
+                                    </h5>
+                                    <p></p>
+                                    <div className="nav small border-top pt-3">
+                                        <Link className="me-2 text-body" to="#"><i className="bi-calendar me-1"></i>개봉일 : {}</Link>
+                                        <Link className="text-body fw-600 ms-auto" to="#">더보기<i className="bi-chevron-right"></i></Link>
                                     </div>
                                 </div>
                             </div>
+                        </div>
                         ))}
+
                         {/*=============반복문 구간 끝=====================*/}
                     </div>
                 </div>
